22f17f57608af7e8627d35dafc9f731f5562060d
Kazuto Sato <kazubonx@gmail.com>
2020-05-05 13:18:24 +0900

    v 0.0.5

---
+++ sample/sample.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <h1>{{message}}</h1>
+    <input type="text" v-model="greeting" />
+  </div>
+</template>
+
+<script>
+import Axios from 'axios';
+
+export default {
+  props: ['name'],
+  data() {
+    return {
+      greeting: '',
+    }
+  },
+  computed: {
+    message() {
+      return `${this.name} さん、 ${this.greeting}`;
+    }
+  }
+}
+</script>
+
+<style>
+h1 { color: #219186 }
+</style>
